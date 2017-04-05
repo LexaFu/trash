ddc19b461d45d572b6f6b7bf78fac46b66c2fe3b
lexa <axel.lequentrec95@gmail.com>
2017-04-05 19:58:26 +0200

    connect2

--- connect.php
+++ connect.php
@@ -2,10 +2,13 @@
 
 try{
     $bdd = new PDO('mysql:host=localhost;dbname=trash;charset=latin1', 'root','root');
+    $bdd = new PDO('mysql:host=localhost;dbname=trash;charset=latin1', 'root','');
 }
 catch(Exeption $e)
 {
     die('Erreur : '.$e->getMessage());
+
+
 }
 
 ?>
