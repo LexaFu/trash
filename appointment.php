6a78dabb9abacb60e567abb30f8c1d3a0b3545c7
Cynthia <cynthiaszarzynski@gmail.com>
2017-04-26 14:55:45 +0200

    ajout

---
+++ appointment.php
@@ -0,0 +1,67 @@
+<?php 
+ include "header.php";
+?>
+
+<form class= "form" action="" method="post">
+   	 		<div class="last_name">
+        		<label for="last_name">Nom</label>
+        		<input type="text" id="last_name" />
+    		</div>
+
+    		<div class="first_name">
+        		<label for="first_name">Prénom</label>
+        		<input type="text" id="first_name" />
+    		</div>
+
+			<div class="phone">
+				<label for="phone">Téléphone</label>
+				<input type="text" name="phone">
+			</div>
+
+    		<div class="address">
+        		<label for="address">Adresse</label>
+        		<input type="text"/>
+    		</div>
+    		<div class="cp">
+        		<label for="cp">Code postal</label>
+        		<input type="text" name="cp"></input>
+    		</div>
+
+			<div class="type">
+			<label for="type">Type d'encombrant</label>
+			<select>
+				<option>Mobilier</option>
+				<option>Mécanique</option>
+				<option>Environnemental</option>
+			</select>
+			</div>
+
+			<div class="size">
+			<label for="size">Taille de l'encombrant</label>
+			<select>
+				<option>Petit</option>
+				<option>Moyen</option>
+				<option>Grand</option>
+			</select>
+			</div>
+
+			<div class="description">
+        		<label for="description">Description</label>
+        		<textarea id="" size="250" rows="10" cols="50"></textarea>
+    		</div>
+
+    		<input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
+                
+            <input type="file" name="picture" id="img" required="true"/>
+
+            <input type=submit value="submit" name="upload"/>
+
+			<div>
+			<label for="date">Date du rendez vous</label>
+				<input type="date" name="">
+			</div>
+
+	 		<div class="bouton">
+        		<button type="submit">Envoyer</button>
+    		</div>
+		</form>
