e87da195f2432b426acbe559c8eedd0dbb7ff940
Cynthia <cynthiaszarzynski@gmail.com>
2017-05-30 15:37:55 +0200

    modification suscribe.php

--- subscribe.php
+++ subscribe.php
@@ -7,22 +7,22 @@
 <div class="subscribe_form">
 	<form action="subscribe_post.php" method="post">
 		<div class="name">
-			<input type="text" id="name" name="last_name" placeholder="Nom" required="required">
+			<input type="text" id="name" name="last_name" placeholder="Nom" required="required" maxlength="20">
 		</div>
 		<div class="surname">
-			<input type="text" id="surname" name="first_name" placeholder="Prénom" required="required">
+			<input type="text" id="surname" name="first_name" placeholder="Prénom" required="required" maxlength="20">
 		</div>
 		<div class="username">
-	        <input type="text" name="username" id="nickname" placeholder="Identifiant" required="required">
+	        <input type="text" name="username" id="nickname" placeholder="Identifiant" required="required" maxlength="20">
         </div>
         <div class="email">
-        	<input type="mail" name="email" placeholder="Adresse email" required="required">
+        	<input type="mail" name="email" placeholder="Adresse email" required="required" maxlength="40">
         </div>
         <div class="phone">
-        	<input type="number" name="phone" placeholder="Numéro De Téléphone" required="required">
+        	<input type="number" name="phone" placeholder="Numéro De Téléphone" pattern="06[0-9]{10}" required="required">
         </div>
 		<div class="password">
-			<input type="password" name="password" placeholder="Mot De Passe" required="required">
+			<input type="password" name="password" placeholder="Mot De Passe" required="required" maxlength="20">
 		</div>
 		<div >
 	        <button class="validate">Valider</button>
