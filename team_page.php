ba1bcce82bb4cfbe26634134d55e2a70ddfba27c
Giovanni <nvgiovanni@gmail.com>
2017-05-30 15:36:42 +0200

    G3005map

--- team_page.php
+++ team_page.php
@@ -35,7 +35,62 @@ while ($donnees = $req->fetch()){
 ?>
 	<div class="map" id="map">
     </div>
-    
+
+<?php 
+	$requete = $bdd->query("SELECT * FROM reporting");
+	$check = $requete->fetchAll(PDO::FETCH_UNIQUE|PDO::FETCH_ASSOC);
+?>
+	
+<script>   
+var geocoder = new google.maps.Geocoder();
+var map;
+var marker;
+
+  // Lors de l'évènement au click de la souris on fait appel à la fonction placerMarker(avec les coordonnées)
+  google.maps.event.addListener(map, 'click', function(event) {
+    placerMarker(event.latLng);
+  });
+
+   // Affiche les marqueurs disponible dans la BDD
+  <?php foreach ($check as $tab){ ?>
+    new google.maps.Marker({
+        position: new google.maps.LatLng(<?php echo $tab["latitude"] ?>, <?php echo $tab["longitude"] ?>),
+        map: map,
+        title: "<?php echo $tab["address"]; ?>"
+    });
+
+<?php } ?>
+}
+ 
+// Fonction qui est appeller lors du click pour placer un marqueur
+function placerMarker(location) {
+
+    if(marker){ // Si le marqueur existe
+        marker.setPosition(location); // Change de position
+        map.setCenter(location); // Puis on centre la map par rapport à la nouvelle position
+    } else {
+        marker = new google.maps.Marker({ // Création du marqueur
+            position : location, // Ajout de sa nouvelle position
+            map : map // Dans la map
+        });
+    }
+    document.getElementById("latitude").value = location.lat(); // Enregistrement latitude dans l'input
+    document.getElementById("longitude").value = location.lng(); // Enregistrement longitude dans l'input
+ 
+    var coogps = new google.maps.LatLng(document.getElementById("latitude").value, document.getElementById("longitude").value); // Par rapport aux input de la latitude et longitude
+    geocoder.geocode({"latLng": coogps}, function(data, status) { // Geocode les données GPS en Adresse Postale
+            if (status == google.maps.GeocoderStatus.OK && data[0]) {
+                  document.getElementById("adresse").value = data[0].formatted_address;
+            } else {
+                alert("Erreur: " + status);
+            }
+      });
+}
+// Lancement de la construction de la carte google map
+google.maps.event.addDomListener(window, 'load', initial);
+</script>
+	
+	
 
 <?php include "footer.php"; ?>
 
