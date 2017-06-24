ae5661068ec6aa31c2f02554f2c83224049a5dbc
Giovanni <nvgiovanni@gmail.com>
2017-06-24 19:27:17 +0200

    G2406CSS2

--- account.php
+++ account.php
@@ -14,17 +14,18 @@ $resultat = $req-> fetch();
 	<div class="dt_u_ctn">
 		<p>Mes informations: </p>
 		<label for="last_name">Nom: </label>
-		<input type="text" name="last_name" id="lst-nm" value="<?php echo $resultat['last_name'];?>"><br>
+		<input type="text" name="last_name" id="lst-nm" value="<?php echo $resultat['last_name'];?>">
 		<label for="first_name">Prénom: </label>
-		<input type="text" name="first_name" id="fst-nm" value="<?php echo $resultat['first_name']?>"><br>
+		<input type="text" name="first_name" id="fst-nm" value="<?php echo $resultat['first_name']?>">
 		<label for="username">Pseudo: </label>
 		<input type="text" name="username" id="usnm" value="<?php echo $resultat['username'];?>">
 		<label for="email">Email: </label>
-		<input type="text" name="email" id="eml" value="<?php echo $resultat['email']?>"><br>
+		<input type="text" name="email" id="eml" value="<?php echo $resultat['email']?>">
 		<label for="phone">Téléphone: </label>
 		<input type="text" name="phone" id="phn" value="<?php echo $resultat['phone']; ?>">	
 	</div>
 	<div class="apt_ctn">
+		<p id="apt_lst">Mes rendez-vous: </p>
 		<?php 
 		if (isset($_GET['date_appointment']) && isset($_GET['hour_appointment'])) {
 		$req = $bdd->prepare('SELECT date_appointment, hour_appointment FROM appointment');
@@ -33,12 +34,12 @@ $resultat = $req-> fetch();
 			'hour_appointment'=>$_GET['hour_appointment']
 			))
 		?>
-
-		<p>Vous avez pris rendez vous pour le <?php echo $_GET['date_appointment']; ?> à <?php echo $_GET['hour_appointment']; ?>.</p>
+		
+		<p>Vous avez un rendez-vous prévu pour le <?php echo $_GET['date_appointment']; ?> à <?php echo $_GET['hour_appointment']; ?>.</p>
 
 		<?php 
 		} else { ?>
-			<p>Vous n'avez pris aucun rendez vous pour le moment</p>
+			<p>Vous n'avez aucun rendez-vous de prévu pour le moment.</p>
 		<?php
 		}
 		?>
--- login.php
+++ login.php
@@ -1,31 +1,16 @@
 <?php require 'header.php' ?>
 
 <div class="login_container">
-    <div class="login_title">
-        <h2>Veuillez vous connecter</h2>
-    </div>
-
-    <div class="login_form">
+    <div class="lgn_ctn">
         <form action="login_post.php" method="post">
-    	    <div class="username">
-    	        <input type="text" name="username" id="nickname" placeholder="Identifiant">
-            </div>
-            <div class="password">
-    	        <input type="password" name="password" id="password" placeholder="Mot de passe">
-            </div>
-            
-            <input type="submit" value="Valider" class="validate">
-            
-            <div class="login-bottom">
-                <div id="login_recover">
-        		  <a href="mdpO.php">mot de passe oublié ?</a>
-                </div>
-
-                <div id="subscribe">
-                    <a href="subscribe.php">s'incrire</a>
-                </div>
-            </div>
+	        <input type="text" name="username" id="nickname" placeholder="Identifiant">
+	        <input type="password" name="password" id="password" placeholder="Mot de passe">
+            <input type="submit" value="Connexion">
+            <a href="mdpO.php">mot de passe oublié ?</a> 
         </form>
+        <div class="subsc_link">
+            <a href="subscribe.php">s'incrire</a>
+        </div>
     </div>
 </div>
 
