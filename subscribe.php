47406fa9ad5e59b3843b494562cf02905b367fae
Cynthia <cynthiaszarzynski@gmail.com>
2017-06-26 16:34:56 +0200

    modif connect.php, footer.php, _subscribe.scss, ie.css, print.css, screen.css, style.css, subscribe.php, team_page.php

--- subscribe.php
+++ subscribe.php
@@ -1,34 +1,30 @@
 <?php require 'header.php'; ?>
-
-
-<div class="subscribe_title">
-    <h2>Veuillez remplir les champs</h2>
-</div>
-
-<div class="subscribe_form">
-	<form action="subscribe_post.php" method="get">
-		<div class="name">
-			<input type="text" id="name" name="last_name" placeholder="Nom" required="required" maxlength="20">
-		</div>
-		<div class="surname">
-			<input type="text" id="surname" name="first_name" placeholder="Prénom" required="required" maxlength="20">
-		</div>
-		<div class="username">
-	        <input type="text" name="username" id="nickname" placeholder="Pseudo (Identifiant)" required="required" maxlength="20">
-        </div>
-        <div class="email">
-        	<input type="email" name="email" placeholder="Adresse email" required="required" maxlength="40">
-        </div>
-        <div class="phone">
-        	<input type="tel" name="phone" placeholder="Numéro De Téléphone" pattern="[0-9]{10}" required="required">
-        </div>
-		<div class="password">
-			<input type="password" name="password" placeholder="Mot De Passe" required="required" maxlength="20">
-		</div>
-		<div >
-	        <button id="validate" class="validate">Valider</button>
-        </div>
-	</form>
+<div class="subscribe_container">
+	<div class="subscribe_ctn">
+		<form action="subscribe_post.php" method="get" class="subscribe_form">
+			<div class="name">
+				<input type="text" id="name" name="last_name" placeholder="Nom" required="required" maxlength="20">
+			</div>
+			<div class="surname">
+				<input type="text" id="surname" name="first_name" placeholder="Prénom" required="required" maxlength="20">
+			</div>
+			<div class="username">
+			    <input type="text" name="username" id="nickname" placeholder="Pseudo (Identifiant)" required="required" maxlength="20">
+		    </div>
+		    <div class="email">
+		        <input type="email" name="email" placeholder="Adresse email" required="required" maxlength="40">
+		    </div>
+		    <div class="phone">
+		        <input type="tel" name="phone" placeholder="Numéro De Téléphone" pattern="[0-9]{10}" required="required">
+		    </div>
+			<div class="password">
+				<input type="password" name="password" placeholder="Mot De Passe" required="required" maxlength="20">
+			</div>
+			<div class="validate">
+			    <input type="submit" value="Valider"></button>
+		    </div>
+		</form>
+	</div>
 </div>
 
 <?php require 'footer.php'; ?>
